015a9393b238ab952387466a8c643970667d8393
“wafflemakr” <wafflemakr@gmail.com>
2019-04-01 22:36:53 -0500

    Updates

---
+++ precache-manifest.8d4568e7baa35ccd1a9d601b8b335e47.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8886af6e13f0dcdc1748",
+    "url": "/react-voting/static/js/runtime~main.2bdb5f4b.js"
+  },
+  {
+    "revision": "12176c59a8e940914749",
+    "url": "/react-voting/static/js/main.7b257c11.chunk.js"
+  },
+  {
+    "revision": "cb2772029b2d1bda4766",
+    "url": "/react-voting/static/js/2.c0f948a5.chunk.js"
+  },
+  {
+    "revision": "cb2772029b2d1bda4766",
+    "url": "/react-voting/static/css/2.2bf9a32d.chunk.css"
+  },
+  {
+    "revision": "0f2424d82aaf9e47f975e9426723fb05",
+    "url": "/react-voting/index.html"
+  }
+];
