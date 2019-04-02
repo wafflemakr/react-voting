1292ff5246664c291fc7e1bf7c0c1653971293f6
“wafflemakr” <wafflemakr@gmail.com>
2019-04-02 00:06:46 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-voting/precache-manifest.ede62b720f8303441e64f6e99bb7468b.js"
+  "/react-voting/precache-manifest.6d3c92e35bf587c2e41bdd53de648749.js"
 );
 
 workbox.clientsClaim();
